412c878c3440f159a14c5fe21d54005575cf9ca2
vincent <vincent.arteaga@gmail.com>
2021-06-07 14:31:58 -0700

    commit tree 8805f094d24ca189f648a7ca23082d4e3a6c3368

--- src/Parent.js
+++ src/Parent.js
@@ -7,18 +7,28 @@ class Parent extends Component {
   constructor() {
     super()
     this.state = {
-      color: getRandomColor()
+      color: getRandomColor(),
+      childrenColor: '#FFF'
     }
+  }
+  // function to change colors
+  changeColor = (newChildColor) => {
+    this.setState({
+      color : getRandomColor(),
+      childrenColor : newChildColor
+    })
   }
 
   render() {
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor} />
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}  />
       </div>
     )
   }
 }
 
 export default Parent
+
+// the onclick goes after the click is called, next to props
